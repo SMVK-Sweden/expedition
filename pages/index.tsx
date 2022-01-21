ca78ab81f5be26e4a36e78b0a13dd7ea1d34e3b3
Nautman <28629647+Nautman@users.noreply.github.com>
2022-01-21 16:58:41 +0100

    Changed name
    For testing purposes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,13 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Vanadis expeditionen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Welcome to Vanadis expeditionen
         </h1>
 
         <p className="mt-3 text-2xl">
