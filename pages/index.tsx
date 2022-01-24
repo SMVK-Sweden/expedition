dc2ca62c3ff95d1cf8b910ca4911af62995450eb
Douglas <28629647+Nautman@users.noreply.github.com>
2022-01-24 16:25:22 +0100

    Basic functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,128 +1,119 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Timeline from '../components/Timeline'
 import Map from '../components/Map'
-import Page from '../shared/types/Page'
 import styles from '../styles/Home.module.css'
 import CalendarData from '../pages/api/calendar_data.json'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-let pages: Page[] = [
-  {title: "home", href: "/"},
-  {title: "about", href: "/about"},
-]
-
-function InfoParagraphs({data} : {data: any}) {
+function InfoParagraphs({ data }: { data: any }) {
   console.log(data)
-  const {Datum, Koordinater, Plats, ...stories} = data; 
+  const { Datum, Koordinater, Plats, ...stories } = data
   // create the paragraps to display info about the day
   return (
-    <div className="prose lg:prose-xl m-12">
+    <div className="prose lg:prose-xl">
       <h3 className="text-gray-700">{Datum}</h3>
       <h2>{Plats}</h2>
-      {Object.keys(stories).map(key => {
-        if (key === "Länk/Objekt") {
+      {Object.keys(stories).map((key) => {
+        if (key === 'Länk/Objekt') {
           return (
             <p className="mb-10">
-              <span className="font-bold">
-                {`${key}: `}
-              </span>
+              <span className="font-bold">{`${key}: `}</span>
               <a href={stories[key]} className="text-blue-500">
                 {stories[key]}
               </a>
             </p>
-          );
+          )
         } else {
           return (
             <div>
               <h4>{key}</h4>
-              <p className="mb-10">
-                {stories[key]}
-              </p>
+              <p className="mb-10">{stories[key]}</p>
             </div>
           )
         }
       })}
     </div>
-  );
+  )
 }
 
+function findBestCoords(index: number): [number, number] {
+  for (let i = index; i > 0; i--) {
+    if (CalendarData[i].Koordinater)
+      return CalendarData[i].Koordinater as [number, number]
+  }
+  for (let i = index; i < CalendarData.length; i++) {
+    if (CalendarData[i].Koordinater)
+      return CalendarData[i].Koordinater as [number, number]
+  }
+  return [0, 0]
+}
 
 export default function Home() {
-  // get the date that was 100 years ago - this implementation
-  // will break in the future and needs improvment
-  let date = new Date()
-  date.setFullYear(date.getFullYear() - 138)
-  const dateString = `${date.getFullYear()}-${("0" + (date.getMonth()+1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}`
-  // search for the date in the records
-  const startCalendarData = CalendarData.find(elem => elem.Datum == dateString)
+  const [index, setIndex] = useState(0)
+  const currentCalendarData = CalendarData[index]
+  const center: [number, number] =
+    currentCalendarData.Koordinater === undefined
+      ? findBestCoords(index)
+      : (currentCalendarData.Koordinater as [number, number])
 
-  // state of this component
-  const [currentCalendarData, setCurrentCalendarData] = useState<Record<string, any> | undefined>(startCalendarData)
+  const markers: [number, number][] = CalendarData.map(
+    (data) => data.Koordinater as [number, number]
+  )
 
-  // functions to change day
-  const incCurrentData = () => {
-    const index = CalendarData.indexOf(currentCalendarData)
-    setCurrentCalendarData(CalendarData[index + 1])
-  }
-  const decCurrentData = () => {
-    const index = CalendarData.indexOf(currentCalendarData)
-    setCurrentCalendarData(CalendarData[index - 1])
-  }
+  const decreaseIndex = () => setIndex(index - 1 < 0 ? 0 : index - 1)
 
-  // render component
+  const increaseIndex = () =>
+    setIndex(
+      index + 1 > CalendarData.length ? CalendarData.length - 1 : index + 1
+    )
+
   return (
     <div>
       <Head>
         <title>Expedition vanadis</title>
         <link rel="icon" href="/ship.png" />
       </Head>
-
       <div className="flex flex-col overflow-hidden">
-        <Navbar pages={pages} logo_url="/ship.png"/>
-        <Map className={styles.homeMap} center={currentCalendarData?.Koordinater} zoom={4} >
-          {({ TileLayer, Marker } : {TileLayer: any; Marker: any}) => (
-            <>
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-              />
-              {
-                CalendarData.map((data) => {
-                  if (data.Koordinater) {
-                    return (
-                      <Marker
-                        position={data.Koordinater}
-                        key={data.Datum}
-                        eventHandlers={
-                          {
-                            click: () => {
-                              setCurrentCalendarData(data)
-                            }
-                          }
-                        }
-                        icon={L.icon({
-                          iconUrl: 'dot.png',
-                          iconSize: [20, 20],
-                        })}
-                      />
-                    )
-                  }
-                })
-              }
-              <Marker position={currentCalendarData?.Koordinater} icon={L.icon({iconUrl: 'ship.png', iconSize: [100, 100]})}></Marker>
-            </>
-          )}
-        </Map>
+        <Map
+          position={center}
+          markers={markers}
+          clickMarker={(i) => setIndex(i)}
+        />
 
-        <Timeline calendarData={CalendarData} currentCalendarData={currentCalendarData} changeCurrentCalendarData={(elem: any) => setCurrentCalendarData(elem)} />
-
+        {/* 
+        <Timeline
+          calendarData={CalendarData}
+          currentCalendarData={currentCalendarData}
+          changeCurrentCalendarData={(elem: any) =>
+            setCurrentCalendarData(elem)
+          }
+        /> */}
         <div className="bg-white max-w-6xl w-full m-auto">
           <div className="flex">
-            <button onClick={decCurrentData} className="flex-auto bg-gray-100 border-1 hover:bg-white transition-all px-4 py-4">igår</button>
-            <button onClick={incCurrentData} className="flex-auto bg-gray-100 border-1 hover:bg-white transition-all px-4 py-4">imorgon</button>
+            <button
+              onClick={decreaseIndex}
+              className="flex-auto bg-gray-100 border-1 hover:bg-white transition-all px-4 py-4"
+            >
+              igår
+            </button>
+            <button
+              onClick={increaseIndex}
+              className="flex-auto bg-gray-100 border-1 hover:bg-white transition-all px-4 py-4"
+            >
+              imorgon
+            </button>
           </div>
-          <div className="">
+          <input
+            className="w-full p-4"
+            type="range"
+            value={index}
+            min={0}
+            max={CalendarData.length}
+            onChange={(e) => setIndex(parseInt(e.target.value))}
+          />
+          <div className="w-full my-12">
             <InfoParagraphs data={currentCalendarData} />
           </div>
         </div>
