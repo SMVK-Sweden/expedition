22443d9dfe18e2a7a2a20ff40cb30c68a4a75e54
simon-rosen <77513078+simon-rosen@users.noreply.github.com>
2022-02-15 19:51:35 +0100

    sdfh

--- pages/images.tsx
+++ pages/images.tsx
@@ -1,6 +1,6 @@
 import KsamsokImages from '../components/KsamsokImages'
 
 export default function Images() {
-       console.log("test again");
+       console.log("test again"); console.log("test")
   return <KsamsokImages></KsamsokImages>
 }
