ebab5abf5c60d91a7b49a7faa6ae1fbceb508257
Samm <smhanna@kth.se>
2022-02-16 13:19:50 +0100

    Gjorde lite ändringar

--- components/Menu/SideBarData.js
+++ components/Menu/SideBarData.js
@@ -3,18 +3,19 @@ import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import * as IoIcons from 'react-icons/Io'
 import * as GiIcons from 'react-icons/Gi'
+import * as GoIcons from 'react-icons/Go'
 import * as FiIcons from 'react-icons/Fi'
 
 export const SidebarData = [
   {
     title: 'Vanadis info',
-    path: '/',
+    path: '/info',
     icon: <GiIcons.GiSailboat />,
     cName: 'nav-text',
   },
   {
     title: 'Kontakta oss',
-    path: '/',
+    path: '/contact',
     icon: <FiIcons.FiPhoneCall />,
     cName: 'nav-text',
   },
@@ -22,6 +23,12 @@ export const SidebarData = [
     title: 'Home',
     path: '/',
     icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'Var är skeppet idag',
+    path: '/position',
+    icon: <GiIcons.GiPaperBoat />,
     cName: 'nav-text',
   },
 ]
---
+++ pages/info.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+      <h1>Info om Vanadis</h1>
+    </div>
+  )
+}
+export default Info
