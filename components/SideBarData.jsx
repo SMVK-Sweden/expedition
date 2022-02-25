820d062c776303b7b098105a9e47a63a7d41fbf3
SMaltin93 <83406279+SMaltin93@users.noreply.github.com>
2022-02-25 15:35:37 +0100

    Test

--- components/SideBarData.jsx
+++ components/SideBarData.jsx
@@ -1,34 +1,31 @@
 import React from 'react'
-import * as FaIcons from 'react-icons/fa'
-import * as AiIcons from 'react-icons/ai'
-import * as IoIcons from 'react-icons/Io'
-import * as GiIcons from 'react-icons/Gi'
-import * as GoIcons from 'react-icons/Go'
-import * as FiIcons from 'react-icons/Fi'
+import { AiFillHome } from 'react-icons/ai'
+import { GiSailboat, GiPositionMarker } from 'react-icons/gi'
+import { FiPhoneCall } from 'react-icons/fi'
 
 export const SidebarData = [
   {
     title: 'Vanadis info',
     path: 'https://sv.wikipedia.org/wiki/Vanadis_v%C3%A4rldsomsegling',
-    icon: <GiIcons.GiSailboat />,
+    icon: <GiSailboat />,
     cName: 'nav-text',
   },
   {
     title: 'Kontakta oss',
     path: 'https://www.etnografiskamuseet.se/besok/kontakta-oss/',
-    icon: <FiIcons.FiPhoneCall />,
+    icon: <FiPhoneCall />,
     cName: 'nav-text',
   },
   {
     title: 'Home',
     path: '/',
-    icon: <AiIcons.AiFillHome />,
+    icon: <AiFillHome />,
     cName: 'nav-text',
   },
   {
     title: 'Var är skeppet idag',
     path: '/position',
-    icon: <GiIcons.GiPositionMarker />,
+    icon: <GiPositionMarker />,
     cName: 'nav-text',
   },
 ]
