afcdadf49fb4bd8b7c2914cd78808798cf57b7f4
Douglas <28629647+Nautman@users.noreply.github.com>
2022-03-01 13:48:59 +0100

    Update .storybook/preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,7 +8,6 @@ Object.defineProperty(NextImage, 'default', {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 })
-// -------------------------
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
