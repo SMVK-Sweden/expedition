afdef66c5de0debce8f91b8c6132554bab8d9815
simon-rosen <simon_rosen96@hotmail.com>
2022-03-01 11:58:14 +0100

    tailwind verkar funka!

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
@@ -9,6 +11,19 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    {
+      // as suggested by https://theodorusclarence.com/blog/nextjs-storybook-tailwind
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: '@storybook/react',
   core: {
@@ -17,10 +32,4 @@ module.exports = {
   typescript: {
     reactDocgen: false,
   },
-  // webpackFinal: async (baseConfig) => {
-  //   const nextConfig = require('/path/to/next.config.js')
-
-  //   // merge whatever from nextConfig into the webpack config storybook will use
-  //   return { ...baseConfig, ...nextConfig }
-  // },
 }
