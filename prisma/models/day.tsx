37a1d42eca7f3d0d1bfa0ce49417e2eea4ba93dc
Simon Rosén <77513078+simon-rosen@users.noreply.github.com>
2022-03-29 20:35:47 +0200

    testar ändra sökningen lite

--- prisma/models/day.tsx
+++ prisma/models/day.tsx
@@ -11,10 +11,11 @@ export interface DayModel {
 }
 
 export async function readDayFromDate(date: Date | string) {
-  const day = await prisma.day.findUnique({
+  const day = await prisma.day.findFirst({
     where: {
-      date: new Date(date),
+      date: { gte: new Date(date) },
     },
+    orderBy: { date: 'asc' },
     include: { diaryEntries: true },
   })
 
