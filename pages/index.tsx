1aa74ae35f881734b71e65ad349fc8a140628953
simon-rosen <simon_rosen96@hotmail.com>
2022-03-31 15:45:01 +0200

    ändrade till routing på client igen

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import todaysDate from './lib/todaysDate'
+
+export default function Index() {
+  const router = useRouter()
+  useEffect(() => {
+    router.push(`/days/${todaysDate()}`)
+  }, [])
+  return <></>
+}
