92e153bd2f7bc952bf05b14dd24832f12536f2d1
simon-rosen <simon_rosen96@hotmail.com>
2022-04-02 12:37:32 +0200

    fixade merge conflicts

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,13 +7,14 @@ import Fetcher from '../components/Fetcher'
 import useSWR from 'swr'
 import info from '../pages/api/about_page.json'
 import SideBar from '../components/SideBar'
+import NavigationBar from '../components/NavigationBar'
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 export default function About() {
   return (
     <div>
-      <SideBar />
+      <NavigationBar />
       <ul>
         <li>
           <Fetcher> </Fetcher>
