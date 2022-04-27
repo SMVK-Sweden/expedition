dcc6a5764e797bdc1abf50ce1ebb0ddc85cbbfbf
simon-rosen <simon_rosen96@hotmail.com>
2022-04-27 10:05:37 +0200

    fixade lite typescript fel

--- lib/api/days.ts
+++ lib/api/days.ts
@@ -4,10 +4,10 @@ import { DayWithDiaryEntries } from '../types/prismaTypes'
 import { yearMonthDay } from '../dateConversion'
 
 // fetch one day
-export function createDay(day: DayWithDiaryEntries) {}
+//export function createDay(day: DayWithDiaryEntries) {}
 
 // if date is a string it is expected to be a yyyy-mm-dd
-export async function getDay(date: Date): DayWithDiaryEntries {
+export async function getDay(date: Date): Promise<DayWithDiaryEntries> {
   if (typeof date === 'string') console.log(date)
   const { data, status } = await axios.get<DayWithDiaryEntries>(
     typeof date === 'string'
@@ -17,12 +17,12 @@ export async function getDay(date: Date): DayWithDiaryEntries {
   return data
 }
 
-export function updateDay(date: Date, updatedDay: DayWithDiaryEntries) {}
+//export function updateDay(date: Date, updatedDay: DayWithDiaryEntries) {}
 
-export function deleteDay(date: Date) {}
+//export function deleteDay(date: Date) {}
 
 // fetch all days
-export async function getDays(): DayWithDiaryEntries[] {
+export async function getDays(): Promise<DayWithDiaryEntries[]> {
   const { data, status } = await axios.get<DayWithDiaryEntries[]>('/api/days', {
     headers: { Accept: 'application/json' },
   })
